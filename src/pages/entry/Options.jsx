abeb611761491a751d4b3c7040aac4bee9495733
tong.yao <tong.yao@check24.de>
2023-04-19 10:48:23 +0200

    write the test for error message

--- src/pages/entry/Options.jsx
+++ src/pages/entry/Options.jsx
@@ -4,9 +4,11 @@ import { useEffect, useState } from 'react';
 import Row from 'react-bootstrap/Row';
 import ScoopOption from './ScoopOption';
 import ToppingOption from './ToppingOption';
+import AlertBanner from '../common/AlertBanner';
 
 export default function Options({ optionType }) {
   const [items, setItems] = useState([]);
+  const [error, setError] = useState(false);
 
   //optionType is 'scoops' or 'toppings'
 
@@ -14,10 +16,12 @@ export default function Options({ optionType }) {
     axios
       .get(`http://localhost:3030/${optionType}`)
       .then((response) => setItems(response.data))
-      .catch((error) => {
-        // Todo: handle error response
-      });
+      .catch((error) => setError(true));
   }, [optionType]);
+
+  if (error) {
+    return <AlertBanner />;
+  }
 
   // TODO: Replace null with ToppingOption when available
   const ItemCompoent = optionType === 'scoops' ? ScoopOption : ToppingOption;
--- src/pages/entry/tests/OrderEntry.test.jsx
+++ src/pages/entry/tests/OrderEntry.test.jsx
@@ -1,29 +1,25 @@
-import { render, screen } from '@testing-library/react';
+import { render, screen, waitFor } from '@testing-library/react';
 import OrderEntry from '../OrderEntry';
 import { server } from '../../../mocks/server';
 import { rest } from 'msw';
 
 test('display error code for scoops and topping', async () => {
-  await server.resetHandlers(
-    rest.get('http://localhost:3030/scoops', (req, res, ctx) => {
-      return res(
-        ctx.status(500),
-        ctx.json({ errorMssage: 'something went wrong' })
-      );
-    }),
-    rest.get('http://localhost:3030/toppings', (req, res, ctx) => {
-      return res(
-        ctx.status(500),
-        ctx.json({ errorMssage: 'something went wrong' })
-      );
-    })
+  server.resetHandlers(
+    rest.get('http://localhost:3030/scoops', (req, res, ctx) =>
+      res(ctx.status(500))
+    ),
+    rest.get('http://localhost:3030/toppings', (req, res, ctx) =>
+      res(ctx.status(500))
+    )
   );
 
   render(<OrderEntry />);
 
-  const allErrorMessage = await screen.findAllByRole('alert', {
-    value: 'something went wrong',
-  });
+  await waitFor(async () => {
+    const allErrorMessage = await screen.findAllByRole('alert', {
+      text: 'An unexpected error happend',
+    });
 
-  expect(allErrorMessage).toHaveLength(2);
+    expect(allErrorMessage).toHaveLength(2);
+  });
 });
